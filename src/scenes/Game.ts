d53a45d2272aa6aff9423e8c72eeb5956d73760e
David Zhang <david8zhang@berkeley.edu>
2021-07-25 23:35:19 -0700

    added perlin noise rendering

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -19,43 +19,40 @@ export default class Game extends Phaser.Scene {
   public static TILE_HEIGHT = 16
   public static TILE_WIDTH = 16
 
+  public static OCEAN_TILE = 38
+  public static SHALLOW_OCEAN_TILE = 25
+  public static WET_SAND_TILE = 12
+  public static DRY_SAND_TIILE = 0
+  public static GRASS_TILE = 49
+
+  public mapScale = 100
+  public offsetX = 0
+  public offsetY = 0
+
   constructor() {
     super('game')
   }
 
   create(): void {
+    this.cursors = this.input.keyboard.createCursorKeys()
     this.initTilemap()
   }
 
   checkTiles() {
-    const map: number[][] = []
-    let tileIndex = 0
-    for (let i = 0; i < 10; i++) {
-      map[i] = new Array(10)
-      for (let j = 0; j < 10; j++) {
-        map[i][j] = tileIndex
-        const text = this.add.text(j * 16, i * 16, tileIndex.toString(), {
-          fontSize: '10px',
-          color: 'black',
-        })
-        tileIndex++
-        text.setDepth(100)
-      }
-    }
-    const offset = { x: 0, y: 0 }
+    const offset = { x: this.offsetX, y: this.offsetY }
     const perlinConfig = {
-      height: 100,
-      width: 100,
-      scale: 10,
+      height: 40,
+      width: 40,
+      scale: this.mapScale,
       octaves: 4,
-      persistence: 0.5,
-      lacunarity: 2,
+      persistence: 0.1,
+      lacunarity: 1,
       seed: 100,
       offset,
     }
     const perlinTileGrid = this.generatePerlinTilegrid(perlinConfig)
-    console.log(perlinTileGrid)
-    return map
+    const tileMap = this.generateTileMapFromPerlinNoise(perlinTileGrid)
+    return tileMap
   }
 
   generatePerlinTilegrid(config: PerlinConfig) {
@@ -65,7 +62,7 @@ export default class Game extends Phaser.Scene {
     let maxNoiseHeight = Number.MIN_SAFE_INTEGER
     let minNoiseHeight = Number.MAX_SAFE_INTEGER
 
-    const prng = new Phaser.Math.RandomDataGenerator()
+    const prng = new Phaser.Math.RandomDataGenerator([seed.toString()])
     const octaveOffsets: { x: number; y: number }[] = []
     for (let i = 0; i < octaves; i++) {
       const offsetX = prng.between(-100000, 100000) + offset.x
@@ -88,7 +85,6 @@ export default class Game extends Phaser.Scene {
           const y = ((j - halfWidth) / scale) * frequency + octaveOffsets[k].y
 
           const perlinValue = noise.perlin2(x, y)
-          console.log(perlinValue)
           noiseHeight += perlinValue * amplitude
           amplitude *= persistence
           frequency *= lacunarity
@@ -101,16 +97,34 @@ export default class Game extends Phaser.Scene {
         tileGrid[i][j] = noiseHeight
       }
     }
-
-    console.log(maxNoiseHeight)
-    console.log(minNoiseHeight)
     for (let i = 0; i < width; i++) {
       for (let j = 0; j < height; j++) {
-        const val = inverseLerp(minNoiseHeight, maxNoiseHeight, tileGrid[i][j])
-        // tileGrid[i][j] = inverseLerp(minNoiseHeight, maxNoiseHeight, tileGrid[i][j])
+        tileGrid[i][j] = inverseLerp(minNoiseHeight, maxNoiseHeight, tileGrid[i][j])
       }
     }
     return tileGrid
+  }
+
+  generateTileMapFromPerlinNoise(perlinTileMap: number[][]) {
+    const map: number[][] = []
+    for (let i = 0; i < perlinTileMap.length; i++) {
+      map[i] = new Array(perlinTileMap[0].length)
+      for (let j = 0; j < perlinTileMap[0].length; j++) {
+        const perlinValue = perlinTileMap[i][j]
+        if (perlinValue >= 0 && perlinValue <= 0.35) {
+          map[i][j] = Game.OCEAN_TILE
+        } else if (perlinValue > 0.35 && perlinValue <= 0.45) {
+          map[i][j] = Game.SHALLOW_OCEAN_TILE
+        } else if (perlinValue > 0.45 && perlinValue <= 0.55) {
+          map[i][j] = Game.WET_SAND_TILE
+        } else if (perlinValue > 0.55 && perlinValue <= 0.65) {
+          map[i][j] = Game.DRY_SAND_TIILE
+        } else if (perlinValue > 0.65 && perlinValue <= 1) {
+          map[i][j] = Game.GRASS_TILE
+        }
+      }
+    }
+    return map
   }
 
   initTilemap() {
@@ -122,5 +136,33 @@ export default class Game extends Phaser.Scene {
     })
     const tileset = this.map.addTilesetImage('beach-tiles', 'beach-tiles')
     const layer = this.map.createLayer(0, tileset, 0, 0)
+  }
+
+  update() {
+    const leftDown = this.cursors.left?.isDown
+    const rightDown = this.cursors.right?.isDown
+    const upDown = this.cursors.up?.isDown
+    const downDown = this.cursors.down?.isDown
+    const spaceDown = this.cursors.space?.isDown
+    if (leftDown) {
+      this.offsetY -= 0.1
+      this.initTilemap()
+    }
+    if (rightDown) {
+      this.offsetY += 0.1
+      this.initTilemap()
+    }
+    if (downDown) {
+      this.offsetX += 0.1
+      this.initTilemap()
+    }
+    if (upDown) {
+      this.offsetX -= 0.1
+      this.initTilemap()
+    }
+    if (spaceDown) {
+      this.mapScale -= 0.2
+      this.initTilemap()
+    }
   }
 }
